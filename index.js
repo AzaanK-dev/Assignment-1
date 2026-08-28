45b91ad1ee40636874986f3494843266cb1d3bee
AzaanK-dev <azaanmehtabk@gmail.com>
2026-08-28 22:59:12 +0500

    Stage 3: create with validation

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import express from "express"
 const app = express()
 const port = 3000
 
+app.use(express.json());
+
 const tasks = [
     {
         id: 1,
@@ -22,15 +24,7 @@ const tasks = [
 ]
 
 
-app.get('/tasks', (req,res)=>{
-    res.json(tasks)
-})
-
-app.get('/tasks/:id', (req,res)=>{
-    const id = req.params.id
-    res.json(tasks.filter(t => t.id==id))
-})
-
+// Stage 1
 app.get('/',(req,res)=>{
     res.json({
         "name": "Task API", 
@@ -41,6 +35,31 @@ app.get('/',(req,res)=>{
 
 app.get('/health',(req,res)=>{
     res.json({ "status": "ok" })
+})
+
+// Stage 2
+app.get('/tasks', (req,res)=>{
+    res.json(tasks)
+})
+
+app.get('/tasks/:id', (req,res)=>{
+    const id = req.params.id
+    res.json(tasks.filter(t => t.id==id))
+}) 
+
+// Stage 3
+app.post('/tasks', (req,res)=>{
+    const task = req.body
+    if(!task.title){
+        return res.status(400).json({message: "Title is missing!"})
+    }
+    const newTask = {
+        id: tasks.length + 1,
+        title: task.title,
+        done: false
+    }
+    tasks.push(newTask)
+    res.status(201).json({message: "Done, here is your receipt"})
 })
 
 app.listen(port, ()=>{
